67d6b8986814931c3b39eceb46e9b2247c93fcdb
inferi0n <rda@apptech-team.ru>
2019-07-07 00:00:21 +0300

    feature: configured pre-push hook and babel for tests

--- tools/scripts/pre-push.bash
+++ tools/scripts/pre-push.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Running pre-push hook"
+<<<<<<< HEAD:tools/scripts/pre-push.bash
 ./tools/scripts/run-tests.bash
+=======
+./scripts/run-tests.bash
+>>>>>>> c0b4069... feature: configured pre-push hook and babel for tests:scripts/pre-push.bash
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
