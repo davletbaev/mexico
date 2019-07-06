b323a3e357259f094d7f146cb786f8205508b435
inferi0n <rda@apptech-team.ru>
2019-07-06 23:33:28 +0300

    feature: added proper image optimizations

--- tools/tasks/images.js
+++ tools/tasks/images.js
@@ -1,11 +1,13 @@
 const gulp             = require('gulp')
 const pump             = require('pump')
+const gulpif           = require('gulp-if')
 const changed          = require('gulp-changed')
 const imagemin         = require('gulp-imagemin')
 const imageminWebp     = require('imagemin-webp')
 const imageminMozjpeg  = require('imagemin-mozjpeg')
 const imageminOptiPng  = require('imagemin-optipng')
-// const responsive       = require('gulp-responsive')
+const responsive       = require('gulp-responsive')
+const spritesmith      = require('gulp.spritesmith')
 
 const paths = require('../paths')
 
@@ -13,54 +15,90 @@ const buildImages = (mode) => (done) => {
   pump([
     gulp.src(paths.src.images),
     ...((mode === 'development') ? [ changed(paths.dist.images) ] : []),
-    // responsive({
-    //   '**/*.png': [{
-    //     width: '50%',
-    //     rename: {
-    //       suffix: '@1x',
-    //     }
-    //   },{
-    //     width: '50%',
-    //     format: 'webp',
-    //     rename: {
-    //       suffix: '@1x'
-    //     }
-    //   },{
-    //     width: '100%',
-    //     rename: {
-    //       suffix: '@2x'
-    //     }
-    //   },{
-    //     width: '100%',
-    //     format: 'webp',
-    //     rename: {
-    //       suffix: '@2x'
-    //     }
-    //   }],
-    //   '**/*.jpg': [{
-    //     width: '50%',
-    //     rename: {
-    //       suffix: '@1x',
-    //     }
-    //   },{
-    //     width: '50%',
-    //     format: 'webp',
-    //     rename: {
-    //       suffix: '@1x'
-    //     }
-    //   },{
-    //     width: '100%',
-    //     rename: {
-    //       suffix: '@2x'
-    //     }
-    //   },{
-    //     width: '100%',
-    //     format: 'webp',
-    //     rename: {
-    //       suffix: '@2x'
-    //     }
-    //   }]
-    // }),
+    responsive({
+      '**/*.png': [
+      {
+        width: 600,
+        progressive: true,
+        rename: {
+          suffix: '@0.5x',
+        }
+      },{
+        width: 600,
+        format: 'webp',
+        rename: {
+          suffix: '@0.5x'
+        }
+      },{
+        width: 1200,
+        progressive: true,
+        rename: {
+          suffix: '@1x',
+        }
+      },{
+        width: 1200,
+        format: 'webp',
+        rename: {
+          suffix: '@1x'
+        }
+      },{
+        width: 2400,
+        progressive: true,
+        rename: {
+          suffix: '@2x'
+        }
+      },{
+        width: 2400,
+        format: 'webp',
+        rename: {
+          suffix: '@2x'
+        }
+      }],
+      '**/*.jpg': [{
+        width: 600,
+        progressive: true,
+        rename: {
+          suffix: '@0.5x',
+          ext: 'jpg'
+        }
+      },{
+        width: 600,
+        format: 'webp',
+        rename: {
+          suffix: '@0.5x'
+        }
+      },{
+        width: 1200,
+        progressive: true,
+        rename: {
+          suffix: '@1x',
+          ext: 'jpg'
+        }
+      },{
+        width: 1200,
+        format: 'webp',
+        rename: {
+          suffix: '@1x'
+        }
+      },{
+        width: 2400,
+        progressive: true,
+        rename: {
+          suffix: '@2x',
+          ext: 'jpg'
+        }
+      },{
+        width: 2400,
+        format: 'webp',
+        rename: {
+          suffix: '@2x'
+        }
+      }]
+    }, {
+      errorOnUnusedConfig: false,
+      skipOnEnlargement: true,
+      errorOnEnlargement: false
+    }),
     ...((mode === 'production') ? [
       imagemin([
         imageminWebp({
